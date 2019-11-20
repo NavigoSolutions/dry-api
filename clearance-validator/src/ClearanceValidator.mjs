d558b942904153ddd37f121dbffa0639c1608c69
Jaroslav Kuboš <jaroslav.kubos@navigo3.com>
2019-11-20 10:11:05 +0100

    Formatting, support for options, namely extra url params

--- clearance-validator/src/ClearanceValidator.mjs
+++ clearance-validator/src/ClearanceValidator.mjs
@@ -1,155 +1,160 @@
 export class ClearanceValidator {
-    static isSet(object, message = undefined) {
-        if (object === null || object === undefined) {
-            throw new Error(message || "Object is null/undefined")
-        }
+  static isSet(object, message = undefined) {
+    if (object === null || object === undefined) {
+      throw new Error(message || "Object is null/undefined")
+    }
+  }
+
+  static isNotSet(object, message = undefined) {
+    if (object !== null && object !== undefined) {
+      throw new Error(message || "Object should be null/undefined but it is set to value")
+    }
+  }
+
+  static isTrue(bool, message = undefined) {
+    if (bool !== true) {
+      throw new Error(message || `True expected, got ${bool}`)
+    }
+  }
+
+  static notBlank(object, message = undefined) {
+    this.isA(object, String)
+
+    if (object === null || object === undefined || object.length < 1) {
+      throw new Error(message || "String should not be blank")
+    }
+  }
+
+  static isA(object, klass, message = undefined) {
+    this.isSet(object)
+    this.isSet(klass)
+
+    if (object.constructor !== klass) {
+      throw new Error(message || `Expected that object is of class ${klass.name} but got ${object.constructor.name}`)
+    }
+  }
+
+  static isFunctionWithArity(func, arity, message = undefined) {
+    if (typeof func !== "function") {
+      throw new Error(`Expected function but got ${typeof func}`)
     }
 
-    static isNotSet(object, message = undefined) {
-        if (object !== null && object !== undefined) {
-            throw new Error(message || "Object should be null/undefined but it is set to value")
-        }
+    if (func.length !== arity) {
+      throw new Error(message || `Expected function with arity ${arity} but got ${func.length}`)
+    }
+  }
+
+  static isFunctionWithArityBetween(func, arityMin, arityMax, message = undefined) {
+    if (typeof func !== "function") {
+      throw new Error(`Expected function but got ${typeof func}`)
     }
 
-    static isTrue(bool, message = undefined) {
-        if (bool !== true) {
-            throw new Error(message || `True expected, got ${bool}`)
-        }
+    if (func.length < arityMin || func.length > arityMax) {
+      throw new Error(
+        message || `Expected function with arity between ${arityMin} and ${arityMax} but got ${func.length}`
+      )
     }
+  }
 
-    static notBlank(object, message = undefined) {
-        this.isA(object, String)
+  static arrayOfSize(array, length, message = undefined) {
+    this.isA(array, Array)
 
-        if (object === null || object === undefined || object.length < 1) {
-            throw new Error(message || "String should not be blank")
-        }
+    if (array.length !== length) {
+      throw new Error(message || `Expected array with length ${length} but got ${array.length}`)
     }
+  }
 
-    static isA(object, klass, message = undefined) {
-        this.isSet(object)
-        this.isSet(klass)
+  static stringOfSize(str, length, message = undefined) {
+    this.isA(str, String)
 
-        if (object.constructor !== klass) {
-            throw new Error(message || `Expected that object is of class ${klass.name} but got ${object.constructor.name}`)
-        }
+    if (str.length !== length) {
+      throw new Error(message || `Expected string with length ${length} but got ${str.length}`)
     }
+  }
 
-    static isFunctionWithArity(func, arity, message = undefined) {
-        if (typeof func !== 'function') {
-            throw new Error(`Expected function but got ${typeof func}`)
-        }
+  static greaterThan(number, ref, message = undefined) {
+    this.isA(number, Number)
+    this.isA(ref, Number)
 
-        if (func.length !== arity) {
-            throw new Error(message || `Expected function with arity ${arity} but got ${func.length}`)
-        }
+    if (number <= ref) {
+      throw new Error(message || `Expected positive number greather than ${ref} but got ${number}`)
     }
+  }
 
-    static isFunctionWithArityBetween(func, arityMin, arityMax, message = undefined) {
-        if (typeof func !== 'function') {
-            throw new Error(`Expected function but got ${typeof func}`)
-        }
+  static lessThan(number, ref, message = undefined) {
+    this.isA(number, Number)
+    this.isA(ref, Number)
 
-        if (func.length < arityMin || func.length > arityMax) {
-            throw new Error(message || `Expected function with arity between ${arityMin} and ${arityMax} but got ${func.length}`)
-        }
+    if (number >= ref) {
+      throw new Error(message || `Expected positive number lesser than ${ref} but got ${number}`)
     }
+  }
 
-    static arrayOfSize(array, length, message = undefined) {
-        this.isA(array, Array)
+  static positiveNumber(number, message = undefined) {
+    this.isA(number, Number)
 
-        if (array.length !== length) {
-            throw new Error(message || `Expected array with length ${length} but got ${array.length}`)
-        }
+    if (number < 0) {
+      throw new Error(message || `Expected positive number but got ${number}`)
     }
+  }
 
-    static stringOfSize(str, length, message = undefined) {
-        this.isA(str, String)
-
-        if (str.length !== length) {
-            throw new Error(message || `Expected string with length ${length} but got ${str.length}`)
-        }
+  static equals(value, ref, message = undefined) {
+    if (value !== ref) {
+      throw new Error(message || `Expected same values but got '${value}' and '${ref}'`)
     }
+  }
 
-    static greaterThan(number, ref, message = undefined) {
-        this.isA(number, Number)
-        this.isA(ref, Number)
+  static nonEmptyArray(array, message = undefined) {
+    this.isA(array, Array)
 
-        if (number <= ref) {
-            throw new Error(message || `Expected positive number greather than ${ref} but got ${number}`)
-        }
+    if (array.length <= 0) {
+      throw new Error(message || `Expected non empty array but got empty`)
     }
+  }
 
-    static lessThan(number, ref, message = undefined) {
-        this.isA(number, Number)
-        this.isA(ref, Number)
+  static arrayOfStrings(array, message = undefined) {
+    this.isA(array, Array)
 
-        if (number >= ref) {
-            throw new Error(message || `Expected positive number lesser than ${ref} but got ${number}`)
-        }
+    array.forEach((item, index) => {
+      if (item === null || item === undefined) {
+        throw new Error(message || `Found null/undefined item on index ${index} in array [${array.join(", ")}]`)
+      }
+
+      if (item.constructor !== String) {
+        throw new Error(
+          message ||
+            `Expected string but got ${object.constructor.name} on index ${index} in array [${array.join(", ")}]`
+        )
+      }
+    })
+  }
+
+  static oneOf(value, options, message = undefined) {
+    this.isA(options, Array)
+
+    if (options.indexOf(value) == -1) {
+      throw new Error(message || `Value '${value}' is not among allowed values ${options.join(", ")}`)
     }
+  }
 
-    static positiveNumber(number, message = undefined) {
-        this.isA(number, Number)
-
-        if (number < 0) {
-            throw new Error(message || `Expected positive number but got ${number}`)
-        }
+  static contained(map, object, message = undefined) {
+    if (!(object in map)) {
+      throw new Error(message || `Object ${object} not found in map`)
     }
+  }
 
-    static equals(value, ref, message = undefined) {
-        if (value !== ref) {
-            throw new Error(message || `Expected same values but got '${value}' and '${ref}'`)
-        }
+  static notContained(map, object, message = undefined) {
+    if (object in map) {
+      throw new Error(message || `Object ${object} is already in map`)
     }
+  }
 
-    static nonEmptyArray(array, message = undefined) {
-        this.isA(array, Array)
+  static regexp(str, regexp, message = undefined) {
+    this.isA(str, String)
+    this.isA(regexp, RegExp)
 
-        if (array.length <= 0) {
-            throw new Error(message || `Expected non empty array but got empty`)
-        }
+    if (!regexp.test(str)) {
+      throw new Error(message || `String "${str}" does not match pattern "${regexp}"`)
     }
-
-    static arrayOfStrings(array, message = undefined) {
-        this.isA(array, Array)
-
-        array.forEach((item, index) => {
-            if (item === null || item === undefined) {
-                throw new Error(message || `Found null/undefined item on index ${index} in array [${array.join(', ')}]`)
-            }
-
-            if (item.constructor !== String) {
-                throw new Error(message || `Expected string but got ${object.constructor.name} on index ${index} in array [${array.join(', ')}]`)
-            }
-        })
-    }
-
-    static oneOf(value, options, message = undefined) {
-        this.isA(options, Array)
-
-        if (options.indexOf(value) == -1) {
-            throw new Error(message || `Value '${value}' is not among allowed values ${options.join(', ')}`)
-        }
-    }
-
-    static contained(map, object, message = undefined) {
-        if (!(object in map)) {
-            throw new Error(message || `Object ${object} not found in map`)
-        }
-    }
-
-    static notContained(map, object, message = undefined) {
-        if ((object in map)) {
-            throw new Error(message || `Object ${object} is already in map`)
-        }
-    }
-
-    static regexp(str, regexp, message = undefined) {
-        this.isA(str, String)
-        this.isA(regexp, RegExp)
-
-        if (!regexp.test(str)) {
-            throw new Error(message || `String "${str}" does not match pattern "${regexp}"`)
-        }
-    }
+  }
 }
